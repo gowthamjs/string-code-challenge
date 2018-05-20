5f6e47786b6097ce3104faa2e6a54bc457ff2bf9
Gowtham J S <gowthamjs@yahoo.com>
2018-05-20 14:14:31 -0700

    refactor tests with data provider

--- src/test/java/com/mastercard/challenge/LongestWordInSentenceTest.java
+++ src/test/java/com/mastercard/challenge/LongestWordInSentenceTest.java
@@ -1,58 +1,34 @@
 package com.mastercard.challenge;
 
-import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class LongestWordInSentenceTest {
 
 	static LongestWordInSentence longestWordInSentence = new LongestWordInSentence();
-	static String inputSentence, expectedLongestWord, actualLongestWord;
 
-	/*
-	 * Test case to validate the longest word, if the input sentence contains
-	 * only one largest word.
-	 */
-	@Test
-	public void shouldReturnLongestWordInSentence() {
-		inputSentence = "The cow jumped over the moon.";
-		actualLongestWord = LongestWordInSentence.findLargestWord(inputSentence);
-		expectedLongestWord = "jumped";
-		validateLongestWordInSentence(expectedLongestWord, expectedLongestWord.length(), actualLongestWord,
-				actualLongestWord.length());
+	@Test(dataProvider = "inputSentence")
+	public void shouldReturnLongestWordInSentence(String inputSentence, List<String> expectedLargestWords) {
+		List<String> actualLargestWords = new LinkedList<>();
+
+		actualLargestWords = LongestWordInSentence.findLargestWord(inputSentence);
+		System.out.println("Expected word" + expectedLargestWords);
+		System.out.println("Actual word" + actualLargestWords);
+		assertTrue(expectedLargestWords.containsAll(actualLargestWords),
+				"Actual longest words are different then expected longest words");
 	}
 
-	/*
-	 * Test case to validate the longest word, if the input sentence is just an
-	 * empty string.
-	 */
-	@Test
-	public void shouldReturnEmptyWord() {
-		inputSentence = "";
-		actualLongestWord = LongestWordInSentence.findLargestWord(inputSentence);
-		expectedLongestWord = "";
-		validateLongestWordInSentence(expectedLongestWord, expectedLongestWord.length(), actualLongestWord,
-				actualLongestWord.length());
+	@DataProvider(name = "inputSentence")
+	public Object[][] createTestData() {
+		return new Object[][] { { "This is great", new LinkedList<String>(Arrays.asList("great")) },
+				{ "", new LinkedList<String>() },
+				{ "This is test for list", new LinkedList<String>(Arrays.asList("This", "test", "list")) },
+				{ "Cool", new LinkedList<String>(Arrays.asList("Cool")) } };
 	}
-
-	/*
-	 * Test case to validate the longest word, if the input sentence has just
-	 * one single word.
-	 */
-	@Test
-	public void shouldReturnInputSentence() {
-		inputSentence = "Challenge";
-		actualLongestWord = LongestWordInSentence.findLargestWord(inputSentence);
-		expectedLongestWord = "Challenge";
-		validateLongestWordInSentence(expectedLongestWord, expectedLongestWord.length(), actualLongestWord,
-				actualLongestWord.length());
-	}
-
-	private void validateLongestWordInSentence(String expectedLongestWord, int expectedLongestwordLength,
-			String actualLongestWord, int actualLongestWordLength) {
-		assertEquals("Actual longest word is different than expected word", expectedLongestWord, actualLongestWord);
-		assertEquals("Length of longest word is different than expected word length", expectedLongestwordLength,
-				actualLongestWord.length());
-	}
-
 }
